cc84f23f66835c7ea7dfd9d0e83d5660f96867bd
leonie-falke <leonie.marie.falke@protonmail.com>
2025-10-25 23:56:52 +0200

    Hashrouter

--- web/src/App.js
+++ web/src/App.js
@@ -1,6 +1,6 @@
 import './style/App.css';
 import React from 'react';
-import { Routes, Route } from 'react-router-dom';
+import { HashRouter as Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
 import HomePage from './pages/home';
