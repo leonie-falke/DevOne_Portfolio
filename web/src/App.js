e0dacf9cd5d4fe1d6da495af9cc56c9dd3619f43
leonie-falke <leonie.marie.falke@protonmail.com>
2025-10-25 22:14:12 +0200

    Fixing again.

--- web/src/App.js
+++ web/src/App.js
@@ -1,5 +1,4 @@
 import './style/App.css';
-import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
--- web/src/pages/home.jsx
+++ web/src/pages/home.jsx
@@ -1,4 +1,3 @@
-import { useRef } from "react";
 import Header from '../components/Header';
 import Projects from "../components/ProjectCard";
 import Skills from "../components/skillBadges";
